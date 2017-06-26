bedddd1bbebba246b274cf9683f6e44f98aeebcd
Alexey Kalinin <alkaline1984@gmail.com>
2017-06-26 16:53:29 -0400

    Changed Port number for Heroku

--- socket.js
+++ socket.js
@@ -3,12 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
 var redis = new Redis();
+const port = process.env.PORT || 8080;
+
 redis.subscribe('factories', function(err, count) {
 });
 redis.on('message', function(channel, message) {
     console.log('Message Received: ' + message + ". Channel: " + channel);
     io.emit(channel, message);
 });
-http.listen(8080, function(){
-    console.log('Listening on Port 8080');
+http.listen(port, function(){
+    console.log('Listening on Port '+ port);
 });
