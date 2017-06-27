e0b70c4251f9d0944954b87075a88858e85f309f
Alexey Kalinin <alkaline1984@gmail.com>
2017-06-26 22:43:08 -0400

    Added proper path for Redis

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
 var host = process.env.REDIS_URL || "127.0.0.1"
-var redis = new Redis('redis://h:p28ef0ee6e0201776a709ee99d37b331dbd0bba904f2e30fcac10a4c1a2cf690c@ec2-34-195-166-91.compute-1.amazonaws.com:46399');
+var redis = new Redis(host);
 const port = process.env.PORT || 8080;
 
 redis.subscribe('factories', function(err, count) {
